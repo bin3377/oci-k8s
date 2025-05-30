eae7cbf89b3df87f2a144b1f9bf05db0977ba9fa
Bin Yi <binyi3377@gmail.com>
2025-05-30 11:40:52 -0700

    ipv6 fix

--- modules/network/gateways.tf
+++ modules/network/gateways.tf
@@ -41,6 +41,61 @@ resource "oci_core_route_table" "igw" {
   }
 }
 
+# NAT Gateway (NGW)
+resource "oci_core_public_ip" "ngw_public_ip" {
+  compartment_id = var.compartment_id
+  display_name   = "${var.prefix}-ngw-reserved-ip"
+  lifetime       = "RESERVED"
+
+  freeform_tags = var.freeform_tags
+  defined_tags  = var.defined_tags
+
+  lifecycle {
+    ignore_changes = [defined_tags, freeform_tags]
+  }
+}
+
+resource "oci_core_nat_gateway" "ngw" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.this.id
+  display_name   = "${var.prefix}-nat-gateway"
+  public_ip_id   = oci_core_public_ip.ngw_public_ip.id
+
+  freeform_tags = var.freeform_tags
+  defined_tags  = var.defined_tags
+
+  lifecycle {
+    ignore_changes = [defined_tags, freeform_tags]
+  }
+}
+
+resource "oci_core_route_table" "ngw" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.this.id
+  display_name   = "${var.prefix}-ngw-route-table"
+
+  route_rules {
+    destination       = local.anywhere
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.ngw.id
+    description       = "NAT Gateway as default gateway"
+  }
+
+  route_rules {
+    destination       = lookup(data.oci_core_services.all_oci_services.services[0], "cidr_block")
+    destination_type  = "SERVICE_CIDR_BLOCK"
+    network_entity_id = oci_core_service_gateway.sgw.id
+    description       = "All Services in region to Service Gateway"
+  }
+
+  freeform_tags = var.freeform_tags
+  defined_tags  = var.defined_tags
+
+  lifecycle {
+    ignore_changes = [defined_tags, freeform_tags]
+  }
+}
+
 # Service Gateway (SGW)
 data "oci_core_services" "all_oci_services" {
   filter {
@@ -87,54 +142,3 @@ resource "oci_core_route_table" "sgw" {
   }
 }
 
-# NAT Gateway (NGW)
-resource "oci_core_public_ip" "ngw_public_ip" {
-  compartment_id = var.compartment_id
-  display_name   = "${var.prefix}-ngw-reserved-ip"
-  lifetime       = "RESERVED"
-
-  freeform_tags = var.freeform_tags
-  defined_tags  = var.defined_tags
-}
-
-resource "oci_core_nat_gateway" "ngw" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.this.id
-  display_name   = "${var.prefix}-nat-gateway"
-
-  public_ip_id = join(",", oci_core_public_ip.ngw_public_ip.*.id)
-
-  freeform_tags = var.freeform_tags
-  defined_tags  = var.defined_tags
-
-  lifecycle {
-    ignore_changes = [defined_tags, freeform_tags]
-  }
-}
-
-resource "oci_core_route_table" "ngw" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.this.id
-  display_name   = "${var.prefix}-ngw-route-table"
-
-  route_rules {
-    destination       = local.anywhere
-    destination_type  = "CIDR_BLOCK"
-    network_entity_id = oci_core_nat_gateway.ngw.id
-    description       = "NAT Gateway as default gateway"
-  }
-
-  route_rules {
-    destination       = lookup(data.oci_core_services.all_oci_services.services[0], "cidr_block")
-    destination_type  = "SERVICE_CIDR_BLOCK"
-    network_entity_id = oci_core_service_gateway.sgw.id
-    description       = "All Services in region to Service Gateway"
-  }
-
-  freeform_tags = var.freeform_tags
-  defined_tags  = var.defined_tags
-
-  lifecycle {
-    ignore_changes = [defined_tags, freeform_tags]
-  }
-}
--- modules/network/vcn.tf
+++ modules/network/vcn.tf
@@ -3,7 +3,7 @@ resource "oci_core_vcn" "this" {
   compartment_id = var.compartment_id
   display_name   = "${var.prefix}-vcn"
   dns_label      = "${var.prefix}vcn"
-  is_ipv6enabled = false
+  is_ipv6enabled = var.enable_ipv6
   cidr_block     = var.vcn_cidr
 
   freeform_tags = var.freeform_tags
